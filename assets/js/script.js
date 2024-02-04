11ae23cc9825573b01aa71127bd16eb5af7bd442
eram m juned <eskengg@gmail.com>
2024-02-04 14:26:47 +0000

    coded to store data in local storage and fetch data from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function initPage() {
 
-    //Variables
+//Variables
     
     var searchButton = $("#search-button");
     var cityList = $(".list-group");
@@ -11,3 +11,27 @@ function initPage() {
     var historyEl = $("#history");
     var fiveDayHeader = $("#five-day");
     var city = [];
+
+//store the cities in local storage
+function storeCity(name){
+    var savedCity = JSON.parse(localStorage.getItem("sCity")) || [] ;
+    savedCity.push(name);
+    localStorage.setItem("sCity",JSON.stringify(savedCity));
+}
+
+// loads the city list from local storage and calls api to get data for last searched city 
+function loadCity() {
+    var cityEl = JSON.parse(localStorage.getItem("sCity"));
+
+    if (cityEl !== null) {
+        cityList = cityEl;
+    }
+
+    renderButtons();
+
+    if (cityList) {
+        var thisCity = cityList[cityList.length - 1]
+        currentWeather(city);
+        forecast(cityid);
+    }
+}
