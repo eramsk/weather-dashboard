1c18c032109dde2c7c68d3aeaf2cece6c94f5fa7
eram m juned <eskengg@gmail.com>
2024-02-04 14:19:56 +0000

    declared variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function initPage() {
+
+    //Variables
+    
+    var searchButton = $("#search-button");
+    var cityList = $(".list-group");
+    var weatherSearch = $(".weather-search");
+    var todayId = $("#today");
+    var forecastId = $("#forecast");
+    var searchInput = $("#search-input");
+    var historyEl = $("#history");
+    var fiveDayHeader = $("#five-day");
+    var city = [];
